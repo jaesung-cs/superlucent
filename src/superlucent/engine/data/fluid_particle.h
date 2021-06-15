7ade85442cfaf7a8b13a1e2961fdfafd54f67059
Jaesung <jaesung.cs@gmail.com>
2021-06-15 23:49:50 +0900

    adding structs for fluid particle simulation

---
+++ src/superlucent/engine/data/fluid_particle.h
@@ -0,0 +1,22 @@
+#ifndef SUPERLUCENT_ENGINE_DATA_FLUID_PARTICLE_H_
+#define SUPERLUCENT_ENGINE_DATA_FLUID_PARTICLE_H_
+
+#include <glm/glm.hpp>
+
+namespace supl
+{
+namespace engine
+{
+struct FluidParticle
+{
+  alignas(16) glm::vec4 prev_position{ 0.f };
+  alignas(16) glm::vec4 position{ 0.f };
+  alignas(16) glm::vec4 velocity{ 0.f };
+  alignas(16) glm::vec4 properties{ 0.f }; // mass
+  alignas(16) glm::vec4 external_force{ 0.f };
+  alignas(16) glm::vec4 color{ 0.f };
+};
+}
+}
+
+#endif // SUPERLUCENT_ENGINE_DATA_FLUID_PARTICLE_H_
