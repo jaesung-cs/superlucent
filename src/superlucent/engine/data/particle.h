3f722efe5eb2bee0301cf57e4a062ae5d526c9f1
Jaesung <jaesung.cs@gmail.com>
2021-06-15 23:38:40 +0900

    particle no longer has radius

--- src/superlucent/engine/data/particle.h
+++ src/superlucent/engine/data/particle.h
@@ -12,7 +12,7 @@ struct Particle
   alignas(16) glm::vec4 prev_position{ 0.f };
   alignas(16) glm::vec4 position{ 0.f };
   alignas(16) glm::vec4 velocity{ 0.f };
-  alignas(16) glm::vec4 properties{ 0.f }; // radius, mass
+  alignas(16) glm::vec4 properties{ 0.f }; // mass
   alignas(16) glm::vec4 external_force{ 0.f };
   alignas(16) glm::vec4 color{ 0.f };
 };
