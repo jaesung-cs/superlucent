91c259ec7942fa54c37b631b0bee14fcea80c188
Jaesung <jaesung.cs@gmail.com>
2021-06-16 23:42:02 +0900

    kernel functions and simulation params glsl files

---
+++ src/superlucent/shader/fluid/simulation_params.glsl
@@ -0,0 +1,15 @@
+layout (binding = 0) uniform FluidSimulatinoParamsSsbo
+{
+  float dt;
+  int num_particles;
+  float epsilon;
+  float wall_offset;
+
+  float h;
+  float radius;
+  float rest_density;
+  float c;
+
+  float k;
+  int n;
+} params;
