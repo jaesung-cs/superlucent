f0e6adfc8abf71be52081cd067d7ec2421c170c6
Jaesung <jaesung.cs@gmail.com>
2021-07-12 21:09:07 +0900

    deleted old files

--- src/superlucent/shader/particle/indirect.glsl
+++
@@ -1,4 +0,0 @@
-layout(std140, binding = 4) buffer DispatchIndirectoCommandSsbo
-{
-  uvec4 dispatch_indirect_commands[];
-};
--- src/superlucent/shader/particle/simulation_params.glsl
+++
@@ -1,9 +0,0 @@
-layout (binding = 1) uniform SimulationParamsUbo
-{
-	float dt;
-	int num_particles;
-  float alpha;
-  float wall_offset;
-
-  float radius;
-} params;
